3533608e5a60b78aad4e7891782bc32495b142f5
s4byek <youexample.com>
2025-12-08 01:53:40 +0300

    third change in dev

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
  class Main {
      public static void main(String[] args) {
-print();
-print2();
+         print();
+         print2();
      }
 
      public static void print() {
@@ -10,6 +10,10 @@ print2();
      }
      public static void print2() {
          System.out.println("Это аналогичные изменения но с другим содержанием для второго коммита");
+
+     }
+     public static void print3() {
+         System.out.println("Это аналогичные изменения но с другим содержанием для третьего коммита");
 
      }
 }
