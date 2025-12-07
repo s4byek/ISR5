2702a2a75c5b760ffdc999a79af39eae1c49a263
s4byek <youexample.com>
2025-12-08 01:51:01 +0300

    first change in dev

---
+++ Main.java
@@ -0,0 +1,10 @@
+ class Main {
+     public static void main(String[] args) {
+
+     }
+
+     public static void print() {
+         System.out.println("Это аналогичные изменения но с другим содержанием для первого коммита");
+
+     }
+}
