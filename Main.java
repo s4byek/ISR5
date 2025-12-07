c453693cab1ae343e1fba765f40bc87dba21f457
s4byek <youexample.com>
2025-12-08 00:26:02 +0300

    third change Main.java

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ class Main{
     public static void main(String[] args) {
         print();
         print2();
+        print3();
     }
     public static void print() {
         System.out.print("Это значительное изменение для первого коммита");
@@ -9,4 +10,8 @@ class Main{
     public static void print2() {
         System.out.print("Это значительное изменение для второго коммита");
     }
+    public static void print3() {
+        System.out.print("Это значительное изменение для третьего коммита");
+    }
+
 }
