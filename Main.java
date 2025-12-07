027ac51deaccfa55a17e82068f4db1ae10731059
s4byek <youexample.com>
2025-12-08 01:52:34 +0300

    second change in dev

--- Main.java
+++ Main.java
@@ -1,10 +1,15 @@
  class Main {
      public static void main(String[] args) {
-
+print();
+print2();
      }
 
      public static void print() {
          System.out.println("Это аналогичные изменения но с другим содержанием для первого коммита");
+
+     }
+     public static void print2() {
+         System.out.println("Это аналогичные изменения но с другим содержанием для второго коммита");
 
      }
 }
