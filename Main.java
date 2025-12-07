2fd31620cc1442f144b721f4ca8c2caf6ef10188
s4byek <youexample.com>
2025-12-08 02:35:05 +0300

    last

--- Main.java
+++ Main.java
@@ -1,25 +1,23 @@
- class Main {
-     public static void main(String[] args) {
-         print();
-         print2();
-         print3();
-         print4();
-     }
+class Main{
+    public static void main(String[] args) {
+        print();
+        print2();
+        print3();
+        print4();
+    }
+    public static void print() {
+        System.out.print("Это аналогичные изменения но с другим содержанием для первого коммита");
+    }
+    public static void print2() {
+        System.out.print("Это аналогичные изменения но с другим содержанием для второго коммита");
+    }
+    public static void print3() {
+        System.out.print("Это аналогичные изменения но с другим содержанием для третьего коммита");
+    }
+    public static void print4() {
+        System.out.print("Это аналогичные изменения но с другим содержанием для четвертого коммита");
+    }
 
-     public static void print() {
-         System.out.println("Это аналогичные изменения но с другим содержанием для первого коммита");
 
-     }
-     public static void print2() {
-         System.out.println("Это аналогичные изменения но с другим содержанием для второго коммита");
 
-     }
-     public static void print3() {
-         System.out.println("Это аналогичные изменения но с другим содержанием для третьего коммита");
-
-     }
-     public static void print4() {
-         System.out.println("Это аналогичные изменения но с другим содержанием для четвертого коммита");
-
-     }
-}
+    }
