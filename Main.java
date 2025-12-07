2028d62a65c5d2788fdcf414941c283deba94a12
s4byek <youexample.com>
2025-12-08 00:24:41 +0300

    second change Main.java

--- Main.java
+++ Main.java
@@ -1,8 +1,12 @@
 class Main{
     public static void main(String[] args) {
         print();
+        print2();
     }
     public static void print() {
         System.out.print("Это значительное изменение для первого коммита");
+    }
+    public static void print2() {
+        System.out.print("Это значительное изменение для второго коммита");
     }
 }
